2ca9082643719314f49f3a0abf3fa83d03ccf16f
Vail Dorchester <vail.dorchester@colorado.edu>
2021-05-11 22:42:53 -0600

    shader takes vec3 now

--- src/systems/rendering/shaders/triangle/vs.rs
+++ src/systems/rendering/shaders/triangle/vs.rs
@@ -1,9 +1,9 @@
 pub static vertex_shader_src: &'static str = r#"
     #version 140
 
-    in vec2 position;
+    in vec3 position;
 
     void main() {
-        gl_Position = vec4(position, 0.0, 1.0);
+        gl_Position = vec4(position, 1.0);
     }
 "#;
