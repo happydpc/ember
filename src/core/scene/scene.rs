c9d6ad599693fcd0ea869a88a2ed69043f2f47e7
Vail Dorchester <vail.dorchester@colorado.edu>
2021-05-14 23:18:07 -0600

    working scene manager

--- src/core/scene/scene.rs
+++ src/core/scene/scene.rs
@@ -1,12 +1,14 @@
 use specs::{World, WorldExt, Builder, Component};
 
+
 pub struct Scene{
     pub world: World,
 }
 
 impl Scene{
-    pub fn initialize(&mut self) {
-        let world = World::new();
-        self.world = world;
+    pub fn create_new() -> Self {
+        Scene{
+            world: World::new(),
+        }
     }
 }
--- src/core/scene/scene_manager.rs
+++ src/core/scene/scene_manager.rs
@@ -1,9 +1,11 @@
+use std::collections::HashMap;
 use super::{super::systems::system::System, scene::Scene};
 
 
 pub struct SceneManager{
-    pub active_scene: Option<Scene>,
-    pub scenes: Vec<Scene>,  // maybe this should be a dictionary so set active scene can take a key and active scene can also be a key?
+    active_scene: Option<i16>,  // Either the scene ID or None
+    scenes: HashMap<i16, Scene>, // Scene ids and scenes
+    scene_counter: i16,
 }
 
 impl System for SceneManager{
@@ -11,23 +13,63 @@ impl System for SceneManager{
         println!("Starting SceneManager...");
     }
     fn shutdown(&mut self){
-        // should this iterate over a scene and save it? probably depends
         self.scenes.clear();
     }
     fn update(&self){
+        if self.active_scene.is_some(){
 
+        }
     }
 }
 
 impl SceneManager{
+    //
+    // public API
+    //
+
+    // creates a new SceneManager
     pub fn create_new() -> Self {
         println!("Creating SceneManager...");
         SceneManager{
             active_scene: None,
-            scenes: Vec::new(),
+            scenes: HashMap::new(),
+            scene_counter: 0,
         }
     }
-    pub fn set_active_scene(&mut self){
+
+    // adds a scene and returns its scene id
+    pub fn add_scene(&mut self, scene: Scene) -> i16 {
+        self.scene_counter+=1;
+        let key = self.scene_counter;
+        self.scenes.insert(key, scene);
+        key
+    }
+
+    // takes a scene id and, if that scene exists, sets that id to be the active scene.
+    pub fn set_active_scene(&mut self, scene_id: i16){
+        match self.scenes.get(&scene_id){
+            Some(x) => self.active_scene = Some(scene_id),
+            None => println!("Scene {} is not in the SceneManager.", scene_id),
+        }
+    }
+
+    // gets the active scene id
+    pub fn get_active_scene_id(&self) -> Option<i16> {
+        self.active_scene
+    }
+
+    //
+    // internal
+    //
+    fn initialize_active_scene(&self){
+        // TODO : Should probably make decisions about scene serialization, what should be serialized,
+        // where things should be serialized etc. then this initialize active scene could actually
+        // load a serialized state. That also makes me wonder if i'm going to want a "wipe scene" function
+        // that clears the serialized state of a scene.
+
+    }
+
+    fn teardown_active_scene(&self){
 
     }
 }
