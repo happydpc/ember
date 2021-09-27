9ec49da3421588e50ef103a6aba2ccbcd70db9de
Vail Dorchester <vail.dorchester@colorado.edu>
2021-09-26 19:36:53 -0600

    implemented framerate and update rate

--- src/core/application/application_manager.rs
+++ src/core/application/application_manager.rs
@@ -7,6 +7,10 @@ use std::{
         RefCell,
         RefMut
     },
+    time::Instant,
+    time::Duration,
+    cmp::Ordering,
+    ops::AddAssign,
 };
 
 use crate::core::{
@@ -57,6 +61,7 @@ pub struct Application{
     surface: Option<Arc<vulkano::swapchain::Surface<winit::window::Window>>>,
 
     log_level: LevelFilter,
+    start_instant: Instant,
 }
 
 impl Manager for Application{
@@ -139,6 +144,7 @@ impl Application{
             event_loop: None,
             surface: None,
             log_level: log_level.unwrap_or(LevelFilter::Info),
+            start_instant: Instant::now(),
         }
     }
 
@@ -147,65 +153,87 @@ impl Application{
         log::info!("Running the application...");
         let event_loop = self.event_loop.take().unwrap();
 
+        // overwrite time
+        log::info!("Startup time: {:?}", Instant::now().duration_since(self.start_instant));
+
+        self.start_instant = Instant::now();
+        let ticks_per_second = 25;
+        let skip_ticks = 1000 / ticks_per_second; // tick every 40 ms
+        let max_frame_skip = 5;
+        let mut loops: u32 = 0;
+        let mut interpolation: f32 = 0.0;
+        let mut next_tick = Instant::now();
+
         event_loop.run(move |event, _, control_flow| {
 
-            match event {
-                Event::WindowEvent { event, .. } => match event {
+            loops = 0;
+            while((Instant::now().cmp(&next_tick) == Ordering::Greater) && loops < max_frame_skip){
+                self.get_physics_manager().unwrap().update();
 
-                    // close requested
-                    WindowEvent::CloseRequested => {
-                        *control_flow = ControlFlow::Exit;
-                    }
+                next_tick.add_assign(Duration::from_millis(skip_ticks));
+                loops = loops + 1;
+            }
 
-                    // window resized
-                    WindowEvent::Resized(_) => {
-                        log::debug!("Window resized...");
-                        match &self.render_manager {
-                            Some(manager) => {
-                                manager.borrow_mut().recreate_swapchain();
-                            },
-                            None => log::error!("Render manager not found when trying to recreate swapchain."),
-                        }
-                    }
-
-                    // keyboard input
-                    WindowEvent::KeyboardInput {
-                        input:
-                            KeyboardInput {
-                                virtual_keycode: Some(virtual_code),
-                                state: ElementState::Pressed,
-                                ..
-                            },
-                        ..
-                        } => {
-                            match &self.input_manager {
-                                Some(manager) => manager.borrow_mut().handle_key_input(Some(virtual_code)),
-                                None => log::error!("Key detected, but no input manager is loaded..."),
-                            };
-                    }
-
-                    // key modifiers, alt, shift, etc
-                    WindowEvent::ModifiersChanged(state) => {
-                        match &self.input_manager{
-                            Some(manager) => manager.borrow_mut().handle_modifier_change(Some(state)),
-                            None => log::error!("Key modifier change detected, but no input manager is loaded..."),
-                        };
-                    }
-
-                    _ => () // catch all for window event
-                }
-
-                Event::RedrawEventsCleared => {
-                    self.run_managers();
-                }
-
-
-                _ => (), // catch all of event match
-            } // end of event match
-        // events have been matched
-
+            // draws and handles events
+            self.handle_event(event, control_flow);
         }); // end of event_loop run
     } // end of run function
+
+    fn handle_event(&mut self, event: winit::event::Event<()>, control_flow: &mut ControlFlow){
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+
+                // close requested
+                WindowEvent::CloseRequested => {
+                    *control_flow = ControlFlow::Exit;
+                }
+
+                // window resized
+                WindowEvent::Resized(_) => {
+                    log::debug!("Window resized...");
+                    match &self.render_manager {
+                        Some(manager) => {
+                            manager.borrow_mut().recreate_swapchain();
+                        },
+                        None => log::error!("Render manager not found when trying to recreate swapchain."),
+                    }
+                }
+
+                // keyboard input
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            virtual_keycode: Some(virtual_code),
+                            state: ElementState::Pressed,
+                            ..
+                        },
+                    ..
+                    } => {
+                        match &self.input_manager {
+                            Some(manager) => manager.borrow_mut().handle_key_input(Some(virtual_code)),
+                            None => log::error!("Key detected, but no input manager is loaded..."),
+                        };
+                }
+
+                // key modifiers, alt, shift, etc
+                WindowEvent::ModifiersChanged(state) => {
+                    match &self.input_manager{
+                        Some(manager) => manager.borrow_mut().handle_modifier_change(Some(state)),
+                        None => log::error!("Key modifier change detected, but no input manager is loaded..."),
+                    };
+                }
+
+                _ => () // catch all for window event
+            }
+
+            Event::MainEventsCleared => {
+                self.render_scene();
+            }
+
+
+            _ => (), // catch all of event match
+        } // end of event match
+    }
 
     pub fn get_scene_manager(&self) -> Option<RefMut<SceneManager>> {
         match &self.scene_manager{
@@ -214,7 +242,14 @@ impl Application{
         }
     }
 
-    fn run_managers(&self){
+    pub fn get_physics_manager(&self) -> Option<RefMut<PhysicsManager>> {
+        match &self.physics_manager {
+            Some(manager) => Some(manager.borrow_mut()),
+            None => None,
+        }
+    }
+
+    fn render_scene(&self){
         match &self.scene_manager {
             Some(scene_manager) => {
                 let scene_manager = scene_manager.borrow_mut();
